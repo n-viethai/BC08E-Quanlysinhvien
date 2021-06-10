d918ea01003992ed2b469f98b76ec546b0bfe4a8
HẢI NGUYỄN <viethai1309@gmail.com>
2021-06-10 17:23:30 +0700

    modified index.js

--- controllers/indexAjax.js
+++ controllers/indexAjax.js
@@ -18,27 +18,21 @@ function LayDanhSachSinhVienApi() {
     method: "GET",
     responseType: "json",
   });
-
   promise.then(function (result) {
     console.log("result", result.data);
     renderTableSinhVien(result.data);
   });
-
   promise.catch(function (error) {
     console.log("error", error);
   });
 }
-
 LayDanhSachSinhVienApi();
 
 function renderTableSinhVien(arrSinhVien) {
   var content = "";
-
   for (var index = 0; index < arrSinhVien.length; index++) {
     var sv = arrSinhVien[index];
-
     var sinhVien = new SinhVien();
-
     sinhVien.maSinhVien = sv.maSinhVien;
     sinhVien.tenSinhVien = sv.tenSinhVien;
     sinhVien.email = sv.email;
@@ -47,7 +41,6 @@ function renderTableSinhVien(arrSinhVien) {
     sinhVien.diemLy = sv.diemLy;
     sinhVien.diemHoa = sv.diemHoa;
     sinhVien.diemRenLuyen = sv.diemRenLuyen;
-
     var trSinhVien = `
               <tr>
                   <td>${sinhVien.maSinhVien}</td>
@@ -71,18 +64,14 @@ function renderTableSinhVien(arrSinhVien) {
                   </td>
               </tr>
           `;
-
     content += trSinhVien;
   }
-
   document.querySelector("#tblSinhVien").innerHTML = content;
 }
 
-//   phương thức POST đưa dữ liệU từ người dùng nhập lên server
-
+//   phương thức POST đưa dữ liệu từ người dùng nhập lên server
 document.querySelector("#themSinhVien").onclick = function () {
   var sinhVien = new SinhVien();
-
   sinhVien.maSinhVien = document.querySelector("#maNhanVien").value;
   sinhVien.tenSinhVien = document.querySelector("#tenNhanVien").value;
   sinhVien.email = document.querySelector("#email").value;
@@ -93,54 +82,44 @@ document.querySelector("#themSinhVien").onclick = function () {
   sinhVien.diemRenLuyen = document.querySelector("#diemRenLuyen").value;
 
   //   gửi dữ liệu về server
-
   var promise = axios({
     url: "http://svcy.myclass.vn/api/SinhVienApi/ThemSinhVien",
     method: "post",
     data: sinhVien,
   });
-
   promise.then(function (result) {
-    // gọi hàm khi truyền dữ liệU lên server thành công
-
+    // gọi hàm khi truyền dữ liệu lên server thành công
     console.log(result.data);
     LayDanhSachSinhVienApi();
   });
-
   promise.catch(function (error) {
     console.log("error", error);
   });
 };
 
 //  phương thức xóa sinh viên ajax
-
 function xoaSinhVien(maSinhVien) {
   var promise = axios({
     url: `http://svcy.myclass.vn/api/SinhVienApi/XoaSinhVien?maSinhVien=${maSinhVien}`,
     method: "DELETE",
   });
-
   promise.then(function (result) {
     LayDanhSachSinhVienApi();
   });
-
   promise.catch(function (error) {
     console.log("error", error);
   });
 }
 
 // chỉnh sửa danh sách sinh viên
-
 function chinhSua(maSinhVien) {
   var promise = axios({
     url: `http://svcy.myclass.vn/api/SinhVienApi/LayThongTinSinhVien?maSinhVien=${maSinhVien}`,
     method: "GET",
   });
-
   promise.then(function (result) {
     var sinhVien = result.data;
     console.log(sinhVien.maSinhVien);
-
     //  đưa các dữ liệu từ server lên các input trên
     document.querySelector("#maNhanVien").value = sinhVien.maSinhVien;
     document.querySelector("#tenNhanVien").value = sinhVien.tenSinhVien;
@@ -152,17 +131,14 @@ function chinhSua(maSinhVien) {
     document.querySelector("#diemHoa").value = sinhVien.diemHoa;
     document.querySelector("#diemRenLuyen").value = sinhVien.diemRenLuyen;
   });
-
   promise.catch(function (error) {
     console.log("error", error);
   });
 }
 
 // cập nhật thông tin sinh viên
-
 document.querySelector("#capNhapThongTin").onclick = function () {
   var sinhVienUpdate = new SinhVien();
-
   sinhVienUpdate.maSinhVien = document.querySelector("#maNhanVien").value;
   sinhVienUpdate.tenSinhVien = document.querySelector("#tenNhanVien").value;
   sinhVienUpdate.email = document.querySelector("#email").value;
@@ -172,18 +148,15 @@ document.querySelector("#capNhapThongTin").onclick = function () {
   sinhVienUpdate.diemLy = document.querySelector("#diemLy").value;
   sinhVienUpdate.diemHoa = document.querySelector("#diemHoa").value;
   sinhVienUpdate.diemRenLuyen = document.querySelector("#diemRenLuyen").value;
-
   var promise = axios({
-      url:`http://svcy.myclass.vn/api/SinhVienApi/CapNhatThongTinSinhVien?maSinhVien=${sinhVienUpdate.maSinhVien}`,
-      method:'PUT',
-      data:sinhVienUpdate
-  })
-
-  promise.then(function(result) {
-      LayDanhSachSinhVienApi();
+    url: `http://svcy.myclass.vn/api/SinhVienApi/CapNhatThongTinSinhVien?maSinhVien=${sinhVienUpdate.maSinhVien}`,
+    method: "PUT",
+    data: sinhVienUpdate,
   });
-
-  promise.catch(function(error) {
-      console.log('error',error);
+  promise.then(function (result) {
+    LayDanhSachSinhVienApi();
+  });
+  promise.catch(function (error) {
+    console.log("error", error);
   });
 };
